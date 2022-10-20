cc40d07dfbe4b054da3163a1ca2dee89d8187444
PC <ilya.zhigulskiy.2000@mail.ru>
2022-10-20 18:33:27 +0400

    new 3

--- my_oop.py
+++ my_oop.py
@@ -7,29 +7,35 @@ class App:
     def __init__(self, source, n):
         self.source = source
         self.n = n
-        self.cap = cv2.VideoCapture(self.source)
-        self.delay = 20
 
     def call_video(self):
+        # self.capimg = self.cap.copy()
+        # self.frame = cv2.imread("Abc.jpg")
+        self.cap = cv2.VideoCapture(self.source)
+        # self.cap.set(3, 480)
+        # self.cap.set(4, 270)
+        self.cap.set(cv2.CAP_PROP_FPS, 100)
         ret, self.frame = self.cap.read()
         self.frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB)
-        self.img = Image.fromarray(self.frame)
+        self.resized = cv2.resize(self.frame, (480, 270), interpolation = cv2.INTER_AREA)
+        self.img = Image.fromarray(self.resized)
+        # self.img = self.img.resize((100,100))
         imgTk = ImageTk.PhotoImage(image=self.img)
         self.label.imgTk = imgTk
         self.label.configure(image = imgTk)
-        self.label.after(self.delay, self.call_video)
+        self.label.after(10, self.call_video)
         # cv2.imshow("frame", self.frame)
         # cv2.waitKey(0)
 
     def call_window(self):
-        self.window = tk.Tk()
-        self.window.geometry("650x450")
-        self.label = tk.Label(self.window)
-        self.label.grid(row=0, column=0)
+        self.label = tk.Label(self.window, relief=tk.RAISED)
+        self.label.pack(side= tk.LEFT)
         self.call_video()
         # self.window.mainloop()
 
     def multi(self):
+        self.window = tk.Tk()
+        self.window.geometry("650x450")
         for i in range(self.n):
             self.call_window()
         self.window.mainloop()
--- my_telemost.py
+++ my_telemost.py
@@ -1,9 +1,6 @@
 from my_oop import App
 import tkinter as tk
 # window = tk.Tk()
-app = App(source = "Abc.jpg", n = 3)
+app = App(source = 0, n = 1)
 app.multi()
 # window.mainloop()
-# Преобразовать изображение в обычный цвет. +
-# Сделать так, чтобы появлялось несколько window, одно с video, дрыгие пустые. В табличку. Отдельные.
-# Free ca
